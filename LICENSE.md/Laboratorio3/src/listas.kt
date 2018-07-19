a24604c35bcc5fd02325a4bdec88035f11d6b4aa
Mirka Monzón <35476538+MirkaNicolle@users.noreply.github.com>
2018-07-19 12:49:20 -0600

    Add files via upload

---
+++ LICENSE.md/Laboratorio3/src/listas.kt
@@ -0,0 +1,42 @@
+package main
+
+println("menu:\n 1. crear lista de tareas\n 2. salir")str
+if (a == 1){
+    println("ingrese el nombre de su nueva lista")
+    arrayListOf(lista)
+    else {
+        println("usted salió")
+    }
+}
+
+println("menu:\n 1. crear lista de tareas\n 2. ver todas las listas de tareas\n 3. seleccionar una lista de tareas\n 4. salir")str
+if (b == 1){
+    println("ingrse el nombre de su nueva lista")str
+}
+if (b == 2){
+
+}
+if (b == 3){
+    println("ingrese el numero de la lista a a seleccionar")str
+}
+if (b == 4) {
+    println("usted salió")
+}
+
+println("1. deseleccionar la lista actual\n 2. agregar una tarea\n 3.Compleatar una tarea\n 4. ver todas las tareas en lista\n 5. salir")str
+if (c == 1){
+
+}
+if (c == 2){
+    println("Ingrese el titulo de la nueva tarea: ")str
+}
+if (c == 3){
+    println("ingrese el numero de la tareas a completar")str
+            println("Tarea completada exitosamente!")
+}
+if (c == 4){
+
+}
+if (c == 5){
+    println("usted salió")
+}
